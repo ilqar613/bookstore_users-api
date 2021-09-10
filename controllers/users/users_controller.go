0206e656b06dbef974bdf29dbbfe6c8027d9299c
ilqar613 <ilqar.mustafazade@hotmail.com>
2021-09-10 10:03:04 +0400

    dao structure

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,51 @@
+package users
+
+import (
+	//"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ilqar613/bookstore_users-api/domain/users"
+	"github.com/ilqar613/bookstore_users-api/services"
+	"github.com/ilqar613/bookstore_users-api/utils/errors"
+)
+
+func CreateUser(c *gin.Context) {
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		//handle json error
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
+}
+
+func GetUser(c *gin.Context) {
+	userId,userErr:=strconv.ParseInt(c.Param("user_id"),10,64)
+	if userErr!=nil{
+		err:=errors.NewBadRequestError("user id should be number")
+		c.JSON(err.Status,err)
+		return
+	}
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
+
+}
+
+func SearchUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me")
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ilqar613/bookstore_users-api/app"
+)
+
+func main() {
+	app.StartApplication()
+}
